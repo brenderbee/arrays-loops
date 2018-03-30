e4d73823a99f4247a4efe2d6a3c7dc7d08961479
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-03-30 10:08:03 -0700

    Add logic to return Boop if number includes a 1.

--- js/scripts.js
+++ js/scripts.js
@@ -7,26 +7,23 @@ function beep(number) {
   for (var i = 0; i <= number; i++){
     if (i % 3 === 0 && i !== 0) {
       resultNumbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (i.toString().includes("1")) {
+      resultNumbers.push("Boop!");
     } else {
       resultNumbers.push(i);
     }
+
+
   }
   return resultNumbers;
 }
-  // resultNumbers.forEach(function(number){
-  //   if (number % 3 === 0) {
-  //    "I'm sorry, Dave. I'm afraid I can't do that."
-  //   }
-  // });
-
-
 
 
 // User-interface Logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    $("ul").empty();
+    $("ul#result").empty();
 
     var inputNumber = parseInt($("#number").val());
 
